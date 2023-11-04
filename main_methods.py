9fc4f6641b302dc05ee2edfee77f4252f430f09c
Riley <riley.i.race@gmail.com>
2023-11-04 13:53:45 -0500

    Create new assignment and new student methods

--- main_methods.py
+++ main_methods.py
@@ -12,7 +12,7 @@ def create_new_student(database, first_name, last_name):
     student_id = r(minimum_id, maximum_id)
     # test if student id already exists
     test = create_db.exists_student(database, student_id)
-    # until a unused student ID is found, get new id
+    # until an unused student ID is found, get new id
     while test is not None:
         student_id = r(minimum_id, maximum_id)
     # insert student into database with first_name, last_name, and random student_id
@@ -26,8 +26,8 @@ def create_new_assignment(database, assignment_name):
     assignment_id = r(minimum_id, maximum_id)
     # test if assignment_id already exists
     test = create_db.exists_assignment(database, assignment_id)
-    # until a unused assignment_id is found, get new id
+    # until an unused assignment_id is found, get new id
     while test is not None:
        assignment_id = r(minimum_id, maximum_id)
     # insert assignment into database with name and assignment_id
-    create_db.insert_student(database, assignment_id)
+    create_db.insert_assignment_name(database, assignment_id, assignment_name)
