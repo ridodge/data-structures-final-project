141f08eba1888afe60375f153bb6d710e058348d
Riley <riley.i.race@gmail.com>
2023-11-06 18:37:51 -0600

    create more methods needed for main methods

--- main_methods.py
+++ main_methods.py
@@ -1,10 +1,11 @@
-import students
-import students_linked_list_queue
+from students import Student
+from students_linked_list_queue import StudentsLinkedQueue as Sq
 import create_db
 from random import randint as r
 
 
 def create_new_student(database, first_name, last_name):
+    """Create a new student"""
     # minimum and maximum of 6-Digit ID
     minimum_id = 100000
     maximum_id = 999999
@@ -18,7 +19,9 @@ def create_new_student(database, first_name, last_name):
     # insert student into database with first_name, last_name, and random student_id
     create_db.insert_student(database, first_name, last_name, student_id)
 
-def create_new_assignment(database, assignment_name):
+
+def create_new_assignment_name(database, assignment_name):
+    """Create a new assignment"""
     # minimum and maximum of 6-Digit ID
     minimum_id = 100000
     maximum_id = 999999
@@ -28,6 +31,31 @@ def create_new_assignment(database, assignment_name):
     test = create_db.exists_assignment(database, assignment_id)
     # until an unused assignment_id is found, get new id
     while test is not None:
-       assignment_id = r(minimum_id, maximum_id)
+        assignment_id = r(minimum_id, maximum_id)
     # insert assignment into database with name and assignment_id
     create_db.insert_assignment_name(database, assignment_id, assignment_name)
+
+
+def test_if_students_exist(database):
+    """Test if the student table is empty"""
+    result = True
+    if len(create_db.select_all_students(database)) == 0:
+        result = False
+    return result
+
+
+def generate_student_linked_list(database):
+    """Create linked list queue from SQL table"""
+    students_list = Sq()
+    # Get all students
+    students = create_db.select_all_students(database)
+    # define index variables
+    id_index, fname_index, lname_index = 0, 1, 2
+    # iterate through students
+    for student in students:
+        # create new student object
+        new_student = Student(student[id_index], student[fname_index], student[lname_index])
+        # enqueue the student object
+        students_list.enqueue(new_student)
+    # return linked list object
+    return students_list
